92701486f1b8f30b7e57c794416920bc03ed6c1c
Manikandan-M <mani.learnmobile@gmail.com>
2012-12-20 03:05:47 +0530

    Log Statement Added

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.helloworld;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+
 
 public class MainActivity extends Activity {
 
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(getClass().getName(), "test");
     }
 
     @Override
